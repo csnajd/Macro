b885daf26aca61fa6ab5ed55ead375ef38c2626e
najd <najdbina@gmail.com>
2026-05-13 17:18:49 +0300

    Initial Commit

---
+++ MacroTests/MacroTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacroTests.swift
+//  MacroTests
+//
+//  Created by najd aljarba on 13/05/2026.
+//
+
+import Testing
+@testable import Macro
+
+struct MacroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
